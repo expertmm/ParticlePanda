b912f374d2ac3a6445a06dd69bc844d13b6d8dc5
expertmm <7557867+expertmm@users.noreply.github.com>
2018-01-08 12:03:23 -0500

    improved Windows run bat

--- Run.bat
+++ Run.bat
@@ -1,1 +1,14 @@
-IF EXIST "C:\Kivy\python.bat" "C:\Kivy\python.bat" main.py 
+@echo off
+IF EXIST "R:\PortableApps\Programming\ParticlePandaPy3" cd R:\PortableApps\Programming\ParticlePandaPy3
+SET SELF_PATH=main.py
+IF EXIST "C:\Kivy\python.bat" "C:\Kivy\python.bat" "%SELF_PATH%"
+IF EXIST "C:\Kivy\python.bat" GOTO END_SILENT
+IF EXIST "C:\Python34\python.exe" "C:\Python34\python.exe" "%SELF_PATH%"
+IF EXIST "C:\Python34\python.exe" GOTO END_SILENT
+IF EXIST "C:\Python35\python.exe" "C:\Python34\python.exe" "%SELF_PATH%"
+IF EXIST "C:\Python35\python.exe" GOTO END_SILENT
+IF EXIST "%SELF_PATH%" python "%SELF_PATH%"
+IF NOT EXIST "%SELF_PATH%" echo You must run this from a drive letter.
+REM echo "Failed to find python."
+pause
+:END_SILENT
